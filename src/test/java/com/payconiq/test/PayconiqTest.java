ceee58658288712f9a741aa40c7f4214ab874256
Vikas <vikaskr0201@gmail.com>
2024-06-03 00:10:49 +0400

    New Test

--- src/test/java/com/payconiq/test/PayconiqTest.java
+++ src/test/java/com/payconiq/test/PayconiqTest.java
@@ -20,7 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
-
+//This is a test
 @RunWith(SerenityRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @WithTags({
